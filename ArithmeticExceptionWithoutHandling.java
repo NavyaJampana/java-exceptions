8ded56633f31178e408a6092988acd3d25fa7c6a
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:40:00 +0530

    Add files via upload

---
+++ ArithmeticExceptionWithoutHandling.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+class ArithmeticExceptionWithoutHandling {
+    public static void main(String[] args) {
+        int result = 10 / 0; 
+        System.out.println("Result: " + result);
+    }
+}
+
